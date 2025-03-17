295826230ec16f31dfcd77423e615c171ebbb4e9
MHR-u <coordinates2016@gmail.com>
2025-03-17 21:04:44 +0300

    app_corbel.py

--- app_corbel.py
+++ app_corbel.py
@@ -120,9 +120,9 @@ if selected_input:
     # Plotting
     fig, ax = plt.subplots(figsize=(8, 6))
     ax.plot(input_range, Vn_values, label=f"Vu vs. {selected_input}", color='blue')
-    ax.set_title(f"Vu vs. {selected_input}")
+    ax.set_title(f"Vn vs. {selected_input}")
     ax.set_xlabel(f"{selected_input} ({description})")
-    ax.set_ylabel("Vu (kN)")
+    ax.set_ylabel("Vn (kN)")
     ax.grid(True)
     ax.legend()
     st.pyplot(fig)
